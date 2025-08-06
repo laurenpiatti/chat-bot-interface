6a8aa5ad7e5bf0fcf329ee0353b82ecf9303c46e
laurenpiatti <lauren.piatti@neudesic.com>
2025-08-06 15:27:48 -0400

    update usings

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./pages/Chat/chat.tsx";
+import App from "./pages/Chat/ChatPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
